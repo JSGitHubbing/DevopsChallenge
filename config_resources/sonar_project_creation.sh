ee7a39b8205e7b3ab96dddada098ebdb48674d34
RHigueras <31961262+al315555@users.noreply.github.com>
2021-01-28 19:32:30 +0100

    Create sonar_project_creation.sh

---
+++ config_resources/sonar_project_creation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -ne 4 ]
+then
+  echo "Please invoke this script with four arguments."
+  echo "create-pipelines <sonarUrlName> <gitOnlinePathRepositoryProject> <user> <password>"
+  exit -1
+fi
+PROJECTREPOSITORYPATH=$2
+SONAR_DOMAIN=$1
+JENKINS_URL=$1
+
+curl --include \
+     --request POST \
+     --header "Content-Type: application/x-www-form-urlencoded" \
+     -u  $3:$4 \
+     -d 'project=$PROJECTREPOSITORYPATH&organization=devossteam&name=myproject' \
+"http://$SONAR_DOMAIN/api/projects/create"
