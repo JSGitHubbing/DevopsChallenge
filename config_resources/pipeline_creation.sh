0ec98e7aeff4b44201c23421ac3043a028a5742e
RHigueras <31961262+al315555@users.noreply.github.com>
2021-02-02 21:27:33 +0100

    Update pipeline_creation.sh

--- config_resources/pipeline_creation.sh
+++ config_resources/pipeline_creation.sh
@@ -11,4 +11,5 @@ REQUEST="http://$JENKINS_URL"
 ENDING='/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)'
 COOKIEJAR="$(mktemp)"
 CRUMB=$(curl -u "$2:$3" --cookie-jar "$COOKIEJAR" "http://$JENKINS_URL$ENDING")
-curl -XPOST "http://$JENKINS_URL/createItem?name=TheProject" -u  $2:$3 --data-binary @config_resources/multipipeline.xml -v --cookie "$COOKIEJAR" -H $CRUMB -H "Content-Type:text/xml"
+curl -XPOST "http://$JENKINS_URL/createItem?name=TheBackProject" -u  $2:$3 --data-binary @config_resources/multipipeline_back.xml -v --cookie "$COOKIEJAR" -H $CRUMB -H "Content-Type:text/xml"
+curl -XPOST "http://$JENKINS_URL/createItem?name=TheFrontProject" -u  $2:$3 --data-binary @config_resources/multipipeline_front.xml -v --cookie "$COOKIEJAR" -H $CRUMB -H "Content-Type:text/xml"
